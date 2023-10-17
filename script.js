ef68a1ff6162551f4abc73fc7090a9251d7938b0
Daniel Klimach Vilela <daniel.klimach.cat@gmail.com>
2023-10-17 16:21:19 -0300

    adicionando randomizacao de palavra

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+let botaoSortear = document.getElementById("btnTips");
+
+document.addEventListener('DOMContentLoaded', function () {
+    realizarSorteio();
+    botaoSortear.addEventListener('click', realizarSorteio);
+
 function sortearPalavra(lista) {
-    let lista[lasdkalsd]
     if (lista.length === 0) {
         console.error("A lista de palavras está vazia.");
         return;
     }
 
+   
+
+    return palavraSorteada;
+}
+
+function realizarSorteio() {
+    let lista = ['banana', 'cafe', 'bolinho', 'suco', 'chocolate','laranja','ovo','pirulito'];
     const indiceAleatorio = Math.floor(Math.random() * lista.length);
     const palavraSorteada = lista[indiceAleatorio];
+    let inputPalavra = document.getElementById("inputPalavra");
+    inputPalavra.value = palavraSorteada;
+    console.log(inputPalavra.value);
 
-    alert("Palavra sorteada: " + palavraSorteada);
 }
+});

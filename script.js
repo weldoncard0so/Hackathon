3d135823b92e738c121eeb2cef35dec6b424f500
Daniel Klimach Vilela <daniel.klimach.cat@gmail.com>
2023-10-17 17:03:16 -0300

    dsads

--- script.js
+++ script.js
@@ -6,11 +6,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function realizarSorteio() {
     let lista = ['banana', 'cafe', 'chocolate','laranja','carne','ovo','pirulito','porco','cachorro','gato',
-'leao','vaca','macaco','cavalo','passaro','girafa','peixe'];
-    const indiceAleatorio = Math.floor(Math.random() * lista.length);
-    const palavraSorteada = lista[indiceAleatorio];
+    'leao','vaca','macaco','cavalo','passaro','girafa','peixe'];
+    let indiceAleatorio = Math.floor(Math.random() * lista.length);
+    let palavraSorteada = lista[indiceAleatorio];
     let inputPalavra = document.getElementById("inputPalavra");
+    let img = document.getElementById("imagem");
     inputPalavra.value = palavraSorteada;
-
+    img.setAttribute("src", "imgs/"+palavraSorteada+".png");
+    fragmentarPalavra(palavraSorteada);
 }
+
+function fragmentarPalavra(palavraSorteada){
+    var palavraFinal = "";
+    for (var i = 0; i < palavraSorteada.length; i++) {
+        var caractere = palavraSorteada[i];
+        if(i%2 == 0 && i!= 0){
+            caractere = "_";
+        }
+        
+        palavraFinal += caractere;
+    }
+
+    inputPalavra.value = palavraFinal;
+    
+}
+
 });

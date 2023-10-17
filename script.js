1fd38321623898101c5d6a727fb8ce483a867ec0
Daniel Klimach Vilela <daniel.klimach.cat@gmail.com>
2023-10-17 16:33:53 -0300

    fouydaSI

--- script.js
+++ script.js
@@ -4,24 +4,12 @@ document.addEventListener('DOMContentLoaded', function () {
     realizarSorteio();
     botaoSortear.addEventListener('click', realizarSorteio);
 
-function sortearPalavra(lista) {
-    if (lista.length === 0) {
-        console.error("A lista de palavras está vazia.");
-        return;
-    }
-
-   
-
-    return palavraSorteada;
-}
-
 function realizarSorteio() {
     let lista = ['banana', 'cafe', 'bolinho', 'suco', 'chocolate','laranja','ovo','pirulito'];
     const indiceAleatorio = Math.floor(Math.random() * lista.length);
     const palavraSorteada = lista[indiceAleatorio];
     let inputPalavra = document.getElementById("inputPalavra");
     inputPalavra.value = palavraSorteada;
-    console.log(inputPalavra.value);
 
 }
 });

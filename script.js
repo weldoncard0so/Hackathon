3f7085821ce37ee34c935d32b8fefda2e4361208
Weldon Cardoso <94559466+weldoncard0so@users.noreply.github.com>
2023-10-17 17:01:26 -0300

    mudança nas imagens e afins

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
     botaoSortear.addEventListener('click', realizarSorteio);
 
 function realizarSorteio() {
-    let lista = ['banana', 'cafe', 'bolinho', 'suco', 'chocolate','laranja','ovo','pirulito'];
+    let lista = ['banana', 'cafe', 'chocolate','laranja','carne','ovo','pirulito','porco','cachorro','gato',
+'leao','vaca','macaco','cavalo','passaro','girafa','peixe'];
     const indiceAleatorio = Math.floor(Math.random() * lista.length);
     const palavraSorteada = lista[indiceAleatorio];
     let inputPalavra = document.getElementById("inputPalavra");

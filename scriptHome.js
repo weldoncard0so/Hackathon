873df33908f9fc9f42ed124087114cefd4679ec4
Weldon Cardoso <94559466+weldoncard0so@users.noreply.github.com>
2023-10-23 17:57:59 -0300

    adicionando rede social nos créditos

--- scriptHome.js
+++ scriptHome.js
@@ -1,3 +1,7 @@
 document.getElementById("btnJogar").addEventListener("click", function() {
     window.location.href = "alfabetizando.html";
   });
+
+  document.getElementById("Creditos").addEventListener("click", function() {
+    window.open("https://www.instagram.com/ghostf4ce.jpg/");
+  });
